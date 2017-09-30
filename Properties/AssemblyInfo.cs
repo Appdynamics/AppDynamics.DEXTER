520d121a8b93314721d2d70ed3e734f0ee7e1983
Daniel Odievich <DanielOdievich@live.com>
2017-09-29 20:21:06 -0700

    Changed to build to x64 binary Release 1.0.0.5

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("AppDynamics")]
-[assembly: AssemblyProduct("DEXTER - Data EXTraction for Enhanced Reporting")]
+[assembly: AssemblyProduct("DEXTER - Data EXTraction and Enhanced Reporting")]
 [assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.4")]
-[assembly: AssemblyFileVersion("1.0.0.4")]
+[assembly: AssemblyVersion("1.0.0.5")]
+[assembly: AssemblyFileVersion("1.0.0.5")]
