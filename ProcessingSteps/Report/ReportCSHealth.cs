a61dd12876f6ca96955fe2245eae30b5ac6bcb1e
zaghani <zain.ghani@appdynamics.com>
2019-09-05 19:00:42 -0400

    Added ReportCSHealth

---
+++ ProcessingSteps/Report/ReportCSHealth.cs
@@ -0,0 +1,99 @@
+﻿using AppDynamics.Dexter.ReportObjectMaps;
+using AppDynamics.Dexter.ReportObjects;
+using OfficeOpenXml;
+using OfficeOpenXml.Drawing.Chart;
+using OfficeOpenXml.Style;
+using OfficeOpenXml.Table;
+using OfficeOpenXml.Table.PivotTable;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace AppDynamics.Dexter.ProcessingSteps
+{
+    public class ReportCSHealth : JobStepReportBase
+    {
+        #region Constants for report contents
+
+        #endregion
+
+        public override bool Execute(ProgramOptions programOptions, JobConfiguration jobConfiguration)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            StepTiming stepTimingFunction = new StepTiming();
+            stepTimingFunction.JobFileName = programOptions.OutputJobFilePath;
+            stepTimingFunction.StepName = jobConfiguration.Status.ToString();
+            stepTimingFunction.StepID = (int)jobConfiguration.Status;
+            stepTimingFunction.StartTime = DateTime.Now;
+            stepTimingFunction.NumEntities = jobConfiguration.Target.Count;
+
+            this.DisplayJobStepStartingStatus(jobConfiguration);
+
+            FilePathMap = new FilePathMap(programOptions, jobConfiguration);
+
+            if (this.ShouldExecute(jobConfiguration) == false)
+            {
+                return true;
+            }
+
+            if (jobConfiguration.Target.Count(t => t.Type == APPLICATION_TYPE_APM) == 0)
+            {
+                return true;
+            }
+
+            try
+            {
+                loggerConsole.Info("Prepare CS Healthcheck Report File");
+
+                #region Health Check
+
+                loggerConsole.Info("List of Health Check");
+
+                #endregion
+
+                loggerConsole.Info("Finalize CS Healthcheck Report File");
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex);
+                loggerConsole.Error(ex);
+
+                return false;
+            }
+            finally
+            {
+                stopWatch.Stop();
+
+                this.DisplayJobStepEndedStatus(jobConfiguration, stopWatch);
+
+                stepTimingFunction.EndTime = DateTime.Now;
+                stepTimingFunction.Duration = stopWatch.Elapsed;
+                stepTimingFunction.DurationMS = stopWatch.ElapsedMilliseconds;
+
+                List<StepTiming> stepTimings = new List<StepTiming>(1);
+                stepTimings.Add(stepTimingFunction);
+                FileIOHelper.WriteListToCSVFile(stepTimings, new StepTimingReportMap(), FilePathMap.StepTimingReportFilePath(), true);
+            }
+        }
+
+        public override bool ShouldExecute(JobConfiguration jobConfiguration)
+        {
+            logger.Trace("Input.Configuration={0}", jobConfiguration.Input.Configuration);
+            loggerConsole.Trace("Input.Configuration={0}", jobConfiguration.Input.Configuration);
+            logger.Trace("Output.Configuration={0}", jobConfiguration.Output.Configuration);
+            loggerConsole.Trace("Output.Configuration={0}", jobConfiguration.Output.Configuration);
+            if (jobConfiguration.Input.Configuration == false || jobConfiguration.Output.Configuration == false)
+            {
+                loggerConsole.Trace("Skipping report of configuration");
+            }
+            return (jobConfiguration.Input.Configuration == true && jobConfiguration.Output.Configuration == true);
+        }
+    }
+}
